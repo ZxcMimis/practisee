ac1977561746cb5ead7ad30af556c39b93a3d9f3
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-09-14 13:02:05 +0300

    style

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -3,6 +3,15 @@ import './Modal.scss'
 
 export class Modal extends Component {
 
+    componentDidMount() {
+        document.addEventListener('keydown', this.keyPress)
+    };
+
+    componentWillMount() {
+        document.addEventListener('keydown', this.keyPress)
+    }
+
+
 
 
     keyPress = (e) => {
@@ -16,7 +25,7 @@ export class Modal extends Component {
         return <div className={this.props.isOpen ? "modalOpen" : "modalClose"}>
             <div className="modal">
                 <h1 className="text">Modal is open</h1>
-                    <button className="btn" onClick={this.props.closeModal}>+</button>
+                <button className="btn" onClick={this.props.closeModal}>+</button>
             </div>
         </div>
     }
